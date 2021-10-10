3f4f59e8b45d0ac0566d01e5a818d31e6c6435d7
Ben Zeckel <Ben.Zeckel@gmail.com>
2021-10-10 12:39:21 -0400

    add simple test of code words like in the book

---
+++ codetest.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+def correctAnswer(words, ir)
+   return words[-2..-1] if ir == words.length
+   return words[0..1] if ir == 1
+   return words[(ir-1)..(ir+1)]
+end
+
+def singleTest(words)
+  ir = rand(words.length)
+  puts "____ #{words[ir]} ____"
+  while(ans = gets.chop.split(" "))
+    break if ans.map(&:downcase) == correctAnswer(words, ir).map(&:downcase)
+    puts "wrong"
+  end
+
+  puts "got it!"
+end
+
+
+filename = "codewords.txt"
+words = IO.readlines(filename).map { |x| x.chop }
+puts "loaded #{words.length} words from #{filename}"
+3.times do singleTest(words) end

9687c02de897d2cc0f0643116e9f32e1e50c53a5
Ben Zeckel <Ben.Zeckel@gmail.com>
2021-10-20 08:56:56 -0400

    comment and work around for ink vs -ing situation

--- numbertest.rb
+++ numbertest.rb
@@ -9,7 +9,7 @@ $vlpa = "\u02A7" # ʧ Voiceless postalveolar affricate, ch sound in chip
 def lookup(c)
    return case c  
      when "d","t" then "1"
-     when "n","ŋ" then "2"
+     when "n" then "2"
      when "m" then "3"
      # r or r colored vowel 
      when "r","ɝ" then "4" 
@@ -19,7 +19,7 @@ def lookup(c)
      #   a little iffy for me vs z sound which would be equal to 0
      when "ʃ",$vpa,$vlpa,"ʒ" then "6" 
      # k or hard g
-     when "k","g" then "7"
+     when "k","g","ŋ" then "7"
      when "v","f" then "8"
      when "p","b" then "9"
      when "z","s" then "0"
@@ -28,10 +28,16 @@ def lookup(c)
 end
 
 def ipa_to_i(wi)
-   r = ""
-   0.upto(wi.length-1) do |i|
-       #puts "#{i}: #{wi[i]} -> #{lookup(wi[i])}"
-       r += lookup(wi[i])
+   r = wi.dup
+   # Roth says ending in ing gives 7 
+   #      but ing sound in ink is supposed to give 2 according to codework example where ink=27
+   #      both cases has sound in IPA of ŋ
+   # for now, special case ink which was probably an oversight by Roth but we might instead special
+   # case ending with ŋ = 7...
+   r.gsub!("ŋk","27")
+   0.upto(r.length-1) do |i|
+       #puts "#{i}: #{r[i]} -> #{lookup(r[i])}"
+       r[i] = lookup(r[i])
    end
 
    return r
